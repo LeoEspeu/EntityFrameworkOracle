45ad77d10e3f0fb90136e5ec4f104a7db032b34d
leo.espeu <btssioleo.espeu@gmail.com>
2018-01-17 09:49:23 +0100

    Debut projet

--- EntityFrameworkOracle/Program.cs
+++ EntityFrameworkOracle/Program.cs
@@ -10,6 +10,23 @@ namespace EntityFrameworkOracle
     {
         static void Main(string[] args)
         {
+            using (OracleEntities oracleContexte = new OracleEntities())
+            {
+                Console.WriteLine("--------------------------");
+                var idEmploye = 33;
+                var requeteEmployesByid = from EMPLOYE in oracleContexte.EMPLOYEs where EMPLOYE.NUMEMP == idEmploye select EMPLOYE;
+
+                var employeId = requeteEmployesByid.FirstOrDefault();
+                if (employeId != null)
+                {
+                    Console.WriteLine(employeId.NOMEMP + " - " + employeId.PRENOMEMP + " - " + employeId.SALAIRE);
+                }
+                else
+                {
+                    Console.WriteLine("L'employé numéro " + idEmploye + " n'existe pas");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
