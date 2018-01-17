3a72f96001937ebbb0bfeffa77b15fac32033c7d
leo.espeu <btssioleo.espeu@gmail.com>
2018-01-17 11:45:35 +0100

    Ajout de nouveau code

--- EntityFrameworkOracle/Program.cs
+++ EntityFrameworkOracle/Program.cs
@@ -12,19 +12,68 @@ namespace EntityFrameworkOracle
         {
             using (OracleEntities oracleContexte = new OracleEntities())
             {
-                Console.WriteLine("--------------------------");
-                var idEmploye = 33;
-                var requeteEmployesByid = from EMPLOYE in oracleContexte.EMPLOYEs where EMPLOYE.NUMEMP == idEmploye select EMPLOYE;
+                Console.WriteLine("---------------------------------------------------------------\n Cours et Séminaires ----------\n---------------------------------------------------------------");
+                //var idEmploye = 33;
+                //var requeteEmployesByid = from EMPLOYE in oracleContexte.EMPLOYEs where EMPLOYE.NUMEMP == idEmploye select EMPLOYE;
 
-                var employeId = requeteEmployesByid.FirstOrDefault();
-                if (employeId != null)
+                //var employeId = requeteEmployesByid.FirstOrDefault();
+                //if (employeId != null)
+                //{
+                //    Console.WriteLine(employeId.NOMEMP + " - " + employeId.PRENOMEMP + " - " + employeId.SALAIRE);
+                //}
+                //else
+                //{
+                //    Console.WriteLine("L'employé numéro " + idEmploye + " n'existe pas");
+                //}
+
+
+                var requete = from c in oracleContexte.COURS join SEMINAIRE in oracleContexte.SEMINAIREs on c.CODECOURS equals SEMINAIRE.CODECOURS select c;
+                var lesCours = requete.Distinct().ToList();
+                foreach (var leCour in lesCours)
                 {
-                    Console.WriteLine(employeId.NOMEMP + " - " + employeId.PRENOMEMP + " - " + employeId.SALAIRE);
+                    Console.WriteLine(leCour.CODECOURS + " - " + leCour.LIBELLECOURS + " - " + leCour.NBJOURS);
+                    foreach (var lesemi in leCour.SEMINAIREs)
+                    {
+                        Console.WriteLine("      " + lesemi.DATEDEBUTSEM);
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("L'employé numéro " + idEmploye + " n'existe pas");
-                }
+
+
+                //var employeProjetNom = from emp in oracleContexte.EMPLOYEs
+                //                       join prj in oracleContexte.PROJETs on emp.CODEPROJET equals prj.CODEPROJET
+                //                       group emp by new { prj.CODEPROJET, prj.NOMPROJET } into groupeEmployes
+                //                       select new
+                //                       {
+                //                           Code = groupeEmployes.Key.CODEPROJET,
+                //                           Nom = groupeEmployes.Key.NOMPROJET,
+                //                           Nombre = groupeEmployes.Count()
+                //                       };
+                //foreach (var ligne in employeProjetNom.ToList())
+                //{
+                //    Console.WriteLine(ligne.Code + " - " + ligne.Nom + " - " + ligne.Nombre);
+                //}
+
+
+                //var emp = oracleContexte.EMPLOYEs.Find(200);
+                //if (emp == null)
+                //{
+                //    Console.WriteLine("L'employé 200 n'existe pas");
+                //}
+                //else
+                //{
+                //    emp.SALAIRE = emp.SALAIRE * (decimal)1.1;
+                //    Console.WriteLine("le nouveau salaire = " + emp.SALAIRE);
+                //    oracleContexte.SaveChanges();
+                //}
+
+
+                //COUR unCour = new COUR();
+                //unCour.CODECOURS = "BR099";
+                //unCour.LIBELLECOURS = "Entity Framework 6 avec Oracle";
+                //unCour.NBJOURS = 4;
+                //oracleContexte.COURS.Add(unCour);
+                //oracleContexte.SaveChanges();
+                //Console.WriteLine("Le cours a été créé");
             }
             Console.ReadLine();
         }
